8cf0067567d0692fcc3444ddbeb18aea9d3f3f20
zzbozheng <zzbozheng@tencent.com>
2019-06-05 22:52:03 +0800

    fix: option.strictEqual can be used for main key.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function recordDiff(beforeRecord = [], afterRecord = [], keyName = 'id', options
   const changed = [];
   const removed = [];
   const changedDetails = [];
+  const { strictEqual = true } = options;
 
   beforeRecordCopy.forEach((item1) => {
     const keyValue1 = item1[keyName];
@@ -13,7 +14,7 @@ function recordDiff(beforeRecord = [], afterRecord = [], keyName = 'id', options
     const isExist = afterRecordCopy.some((item2, index2) => {
       const keyValue2 = item2[keyName];
 
-      if (keyValue1 === keyValue2) {
+      if (keyValue1 === keyValue2 || (!strictEqual && keyValue1 == keyValue2)) {
         afterRecordCopy.splice(index2, 1);
         const diffResult = diff(item1, item2, null, options);
 
